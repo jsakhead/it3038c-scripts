aa40c4578f2292985350687e9be59384377b7bb8
jsakhead <sacketjr@mail.uc.edu>
2022-09-14 13:13:32 -0400

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,9 @@
+﻿$Hello = "Hello, Powershell"
+$IP = getIP
+Write-Host($Hello)
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+function getIP{
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+write-host("This machine's IP is $IP")
+write-host("This machine's IP is {0}" -f $IP)
