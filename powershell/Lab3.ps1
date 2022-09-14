f4aff2b5cb0fa6111e8932ad076b1ad7c74c7c01
jsakhead <sacketjr@mail.uc.edu>
2022-09-14 17:10:19 -0400

    powershell files

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,8 @@
+﻿$IP = (getIP)
+$date = (get-date)
+$PSVersion = $PSVersionTable.PSVersion.Major
+function getIP{
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+$BODY = "This machine's IP is $IP, User is $env:USERNAME, Hostname is $env:COMPUTERNAME. Powershell version $PSVersion, Today's date is $date "
+Send-MailMessage -To "leonard@ucmail.uc.edu" -From "jsackett2012@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
