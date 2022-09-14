370993c137a973718bdf5b77af5998f23c15273e
Joe Sackett <97535141+jsakhead@users.noreply.github.com>
2022-09-14 17:17:16 -0400

    Update Lab3.ps1

--- powershell/Lab3.ps1
+++ powershell/Lab3.ps1
@@ -5,4 +5,4 @@ function getIP{
     (Get-NetIPAddress).ipv4address | Select-String "192*"
 }
 $BODY = "This machine's IP is $IP, User is $env:USERNAME, Hostname is $env:COMPUTERNAME. Powershell version $PSVersion, Today's date is $date "
-Send-MailMessage -To "leonard@ucmail.uc.edu" -From "jsackett2012@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "jsackett2012@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
