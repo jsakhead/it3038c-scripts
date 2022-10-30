5e127e4ace75563b4fdc129f0ffaf316b2f9fc27
Joe Sackett <97535141+jsakhead@users.noreply.github.com>
2022-10-30 15:41:13 -0400

    Update project2.py

--- projects/project2.py
+++ projects/project2.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 import pytz
 
-#timezone = input("Enter USA Timezone:")
-
 us_central = datetime.now(pytz.timezone('US/Central'))
 us_eastern = datetime.now(pytz.timezone('US/Eastern'))
 us_mountain = datetime.now(pytz.timezone('US/Mountain'))
@@ -14,21 +12,24 @@ while True:
     timezone = input("Enter USA Timezone:")
     if timezone == 'Central' or timezone == 'central':
         print('US Central Date/Time', us_central)
-        break
     elif timezone == 'Eastern' or timezone == 'eastern':
         print('US Eastern Date/Time', us_eastern)
-        break
     elif timezone == 'Mountain' or timezone == 'mountain':
         print('US Mountain Date/Time', us_mountain)
-        break
     elif timezone == 'Pacific' or timezone == 'pacific':
         print('US Pacific Date/Time', us_pacific)
-        break
     elif timezone == 'Alaska' or timezone == 'alaska':
         print('US Alaska Date/Time', us_alaska)
-        break
     elif timezone == 'Hawaii' or timezone == 'hawaii':
         print('US Hawaii Date/Time', us_hawaii)
-        break
     else:
         print('Please enter valid USA time zone')
+    restart =input("Would you like to continue? Please type y for yes or n for no")
+    if restart =='y':
+        continue
+    if restart =='n':
+        print('Goodbye :)')
+        break
+    if restart !='y' or 'n':
+        print('incorrect value entered')
+        break 
